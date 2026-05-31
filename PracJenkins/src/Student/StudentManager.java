b8f9bd20afa5cd31b7ced96f754e91768e9ed26e
HiJuwon <wizz2176ytr@naver.com>
2026-06-01 00:15:58 +0900

    [CHORE] 주석 일부 삭제

--- PracJenkins/src/Student/StudentManager.java
+++ PracJenkins/src/Student/StudentManager.java
@@ -22,7 +22,7 @@ public class StudentManager {
         students.remove(name);
     }
     
-    //학생 조회(boolean타입 반환)
+    //학생 조회
     public boolean hasStudent(String name) {
         return students.contains(name);
     }
